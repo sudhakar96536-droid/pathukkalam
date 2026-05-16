c0eb9c30079e878f79f4263a6c801f6e3acbbd7f
sudhakar96536-droid <sudhakar96536@gmail.com>
2026-05-16 16:29:00 +0530

    Update sw.js

--- static/sw.js
+++ static/sw.js
@@ -1,19 +1,27 @@
-const CACHE_NAME = 'social-app-v1';
+const CACHE_NAME = 'pathukkalam-v1';
 
 const urlsToCache = [
     '/',
+    '/static/style.css',
+    '/static/app.js'
 ];
 
 self.addEventListener('install', event => {
+
     event.waitUntil(
         caches.open(CACHE_NAME)
-            .then(cache => cache.addAll(urlsToCache))
+            .then(cache => {
+                return cache.addAll(urlsToCache);
+            })
     );
 });
 
 self.addEventListener('fetch', event => {
+
     event.respondWith(
         caches.match(event.request)
-            .then(response => response || fetch(event.request))
+            .then(response => {
+                return response || fetch(event.request);
+            })
     );
 });
